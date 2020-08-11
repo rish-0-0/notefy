5c384bbf2da7dffbdab4823f0b88b81d7cad607b
Rishabh Anand <rishab@caarya.in>
2020-08-11 10:46:19 +0530

    on notification handler

--- redux/notifications.js
+++ redux/notifications.js
@@ -1,9 +1,10 @@
 import NotifService from '../config/NotificationService';
 import {notificationTypes} from './action_types';
 import store from '../config/store';
+import {ToastAndroid, Platform, Alert} from 'react-native';
 
 const onRegister = (token) => {
-  console.log(token);
+  console.log('Registration Token', token);
   store.dispatch({
     type: notificationTypes.NOTIFICATION_ON_REGISTER,
     payload: {
@@ -12,8 +13,17 @@ const onRegister = (token) => {
   });
 };
 
+const onNotfication = (notification) => {
+  console.log('on Notification', notification);
+  if (Platform.OS === 'ios') {
+    Alert.alert(notification.title);
+  } else if (Platform.OS === 'android') {
+    ToastAndroid.show(notification.title, ToastAndroid.SHORT);
+  }
+};
+
 const initState = {
-  notif_service: new NotifService((token) => onRegister(token)),
+  notif_service: new NotifService((token) => onRegister(token), onNotfication),
   token: null,
 };
 
